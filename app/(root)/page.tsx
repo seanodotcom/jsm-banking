f115dacf8076d7b56a1ecb882531bbd7f0dc789d
Sean O <seanodotcom@yahoo.com>
2024-06-30 23:18:59 -0400

    Implement Balance with Countup & Doughnut Chart

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,27 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName: 'Sean' }
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access & manage your account and transactions"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.37}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
